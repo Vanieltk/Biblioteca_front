f92650109e60b1c81ab34bd5a1900e961dab2fca
ThiagoHABarros <thiagohenrique.b@live.com>
2021-12-29 11:34:41 -0300

    Criação home e refatoração tela login

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Login from './views/Login';
+import Home from './views/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <Login/>
   </React.StrictMode>,
   document.getElementById('root')
 );
